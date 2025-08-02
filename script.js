1e40eaea086786413d9ebfd127a44509656d0301
Andrei Barros <andreibmm@gmail.com>
2025-08-02 12:43:08 -0300

    Correção da função gerar calendário

--- script.js
+++ script.js
@@ -277,76 +277,90 @@ function exportarExcel() {
   XLSX.utils.book_append_sheet(wb, wsDados, 'Escala');
 
   function gerarCalendario(mes, ano, titulo, incluirEletivas = false) {
-    const ws = XLSX.utils.aoa_to_sheet([]);
-    XLSX.utils.sheet_add_aoa(ws, [[`${nomesMes[mes]} / ${ano}`]], { origin: -1 });
-    XLSX.utils.sheet_add_aoa(ws, [[``]], { origin: -1 });
-    XLSX.utils.sheet_add_aoa(ws, [['Horário', 'Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']], { origin: -1 });
+  const ws = XLSX.utils.aoa_to_sheet([]);
+  XLSX.utils.sheet_add_aoa(ws, [[`${nomesMes[mes]} / ${ano}`]], { origin: -1 });
+  XLSX.utils.sheet_add_aoa(ws, [[``]], { origin: -1 });
+  XLSX.utils.sheet_add_aoa(ws, [['Horário', 'Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']], { origin: -1 });
 
-    let diaAtual = new Date(ano, mes, 1);
-    const ultimaData = new Date(ano, mes + 1, 0);
+  const ultimaData = new Date(ano, mes + 1, 0);
+  let diaAtual = new Date(ano, mes, 1);
 
-    while (diaAtual <= ultimaData) {
-      const linhaDias = [''];
-      const semana = [];
-      for (let i = 0; i < 7; i++) {
-        if (diaAtual.getMonth() === mes) {
-          linhaDias.push(diaAtual.getDate());
-          semana.push(diaAtual.toISOString().split('T')[0]);
-        } else {
-          linhaDias.push('');
-          semana.push(null);
-        }
+  while (diaAtual <= ultimaData) {
+    const linhaDias = [''];
+    const semana = [];
+
+    const offset = diaAtual.getDay(); // dia da semana do primeiro dia da semana atual (0 = domingo)
+
+    // Preenche espaços em branco antes do primeiro dia da semana
+    for (let i = 0; i < offset; i++) {
+      linhaDias.push('');
+      semana.push(null);
+    }
+
+    // Preenche os dias restantes da semana
+    for (let i = offset; i < 7; i++) {
+      if (diaAtual > ultimaData) {
+        linhaDias.push('');
+        semana.push(null);
+      } else {
+        linhaDias.push(diaAtual.getDate());
+        semana.push(diaAtual.toISOString().split('T')[0]);
         diaAtual.setDate(diaAtual.getDate() + 1);
       }
-      XLSX.utils.sheet_add_aoa(ws, [linhaDias], { origin: -1 });
-
-      const linhaDiurno = incluirEletivas ? ['07h-19h'] : ['07h-19h'];
-      const linhaNoturno = ['19h-07h'];
-
-      semana.forEach(dataStr => {
-        const pl = plantoesManuais[dataStr] || {};
-        if (incluirEletivas) {
-          linhaDiurno.push(pl.eletiva || '');
-        } else {
-          linhaDiurno.push(pl.diurno || '');
-          linhaNoturno.push(pl.noturno || '');
-        }
-      });
-
-      XLSX.utils.sheet_add_aoa(ws, [linhaDiurno], { origin: -1 });
-      if (!incluirEletivas) XLSX.utils.sheet_add_aoa(ws, [linhaNoturno], { origin: -1 });
     }
 
-    const range = XLSX.utils.decode_range(ws['!ref']);
-    ws['!rows'] = [];
-    for (let R = 0; R <= range.e.r; R++) {
-      if (R === 3) {
-        ws['!rows'].push({ hpt: 25 });
-        for (let C = 0; C <= range.e.c; C++) {
-          const cellRef = XLSX.utils.encode_cell({ r: R, c: C });
-          if (ws[cellRef]) {
-            ws[cellRef].s = {
-              font: { bold: true, italic: true, sz: 16 }
-            };
-          }
-        }
-      } else if (R > 3) {
-        ws['!rows'].push({ hpt: 20 });
-        for (let C = 0; C <= range.e.c; C++) {
-          const cellRef = XLSX.utils.encode_cell({ r: R, c: C });
-          if (ws[cellRef] && typeof ws[cellRef].v === 'string') {
-            ws[cellRef].s = {
-              font: { sz: 14 }
-            };
-          }
-        }
+    XLSX.utils.sheet_add_aoa(ws, [linhaDias], { origin: -1 });
+
+    // Agora monta as linhas de plantões (diurno / noturno / eletiva)
+    const linhaDiurno = incluirEletivas ? ['07h-19h'] : ['07h-19h'];
+    const linhaNoturno = ['19h-07h'];
+
+    semana.forEach(dataStr => {
+      const pl = plantoesManuais[dataStr] || {};
+      if (incluirEletivas) {
+        linhaDiurno.push(pl.eletiva || '');
       } else {
-        ws['!rows'].push({});
+        linhaDiurno.push(pl.diurno || '');
+        linhaNoturno.push(pl.noturno || '');
       }
-    }
+    });
 
-    XLSX.utils.book_append_sheet(wb, ws, titulo);
+    XLSX.utils.sheet_add_aoa(ws, [linhaDiurno], { origin: -1 });
+    if (!incluirEletivas) XLSX.utils.sheet_add_aoa(ws, [linhaNoturno], { origin: -1 });
   }
+
+  // Estilização
+  const range = XLSX.utils.decode_range(ws['!ref']);
+  ws['!rows'] = [];
+  for (let R = 0; R <= range.e.r; R++) {
+    if (R === 3) {
+      ws['!rows'].push({ hpt: 25 });
+      for (let C = 0; C <= range.e.c; C++) {
+        const cellRef = XLSX.utils.encode_cell({ r: R, c: C });
+        if (ws[cellRef]) {
+          ws[cellRef].s = {
+            font: { bold: true, italic: true, sz: 16 }
+          };
+        }
+      }
+    } else if (R > 3) {
+      ws['!rows'].push({ hpt: 20 });
+      for (let C = 0; C <= range.e.c; C++) {
+        const cellRef = XLSX.utils.encode_cell({ r: R, c: C });
+        if (ws[cellRef] && typeof ws[cellRef].v === 'string') {
+          ws[cellRef].s = {
+            font: { sz: 14 }
+          };
+        }
+      }
+    } else {
+      ws['!rows'].push({});
+    }
+  }
+
+  XLSX.utils.book_append_sheet(wb, ws, titulo);
+}
+
 
   const mes = parseInt(document.getElementById('mes').value);
   const ano = parseInt(document.getElementById('ano').value);
